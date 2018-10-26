93195640f59abaa9c171680e50be9a30af4d391f
patricksebastien <patrick@11h11.com>
2018-10-25 22:40:14 -0400

    tac startup touchegg

--- tac.sh
+++ tac.sh
@@ -3,5 +3,6 @@ sleep 5
 pd -rt -jack -channels 2 -open /home/psc/11h11/tac/main.pd &
 sleep 5
 wmctrl -r test.pd -t 1 &
-wmctrl -r test.pd -b toggle,fullscreen
-wmctrl -r theremin.xcf -b toggle,fullscreen
+wmctrl -r test.pd -b toggle,fullscreen &
+wmctrl -r theremin.xcf -b toggle,fullscreen &
+touchegg &
