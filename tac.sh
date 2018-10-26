1e2cab0cc736a992801664ebc2cdb8b98deffea3
patricksebastien <patrick@11h11.com>
2018-10-25 22:49:45 -0400

    window control with right name

--- tac.sh
+++ tac.sh
@@ -2,7 +2,7 @@ flatpak run org.gimp.GIMP//stable /home/psc/11h11/thereminacrayon/gimp/tac.xcf &
 sleep 5
 pd -rt -jack -channels 2 -open /home/psc/11h11/thereminacrayon/puredata/main.pd &
 sleep 5
-wmctrl -r test.pd -t 1 &
-wmctrl -r test.pd -b toggle,fullscreen &
-wmctrl -r theremin.xcf -b toggle,fullscreen &
+wmctrl -r main.pd -t 1 &
+wmctrl -r main.pd -b toggle,fullscreen &
+wmctrl -r tac.xcf -b toggle,fullscreen &
 touchegg &
