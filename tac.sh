56db303956e94cd69a045a09b4bf30a976e31726
patricksebastien <patrick@11h11.com>
2018-10-29 22:31:48 -0400

    adding midi to pd

--- tac.sh
+++ tac.sh
@@ -1,9 +1,12 @@
+sudo service mysql stop
+sudo service nginx stop
+sudo service php7.2-fpm stop
 sleep 5
 bitwig-studio &
 sleep 5
 flatpak run org.gimp.GIMP//stable /home/psc/11h11/thereminacrayon/gimp/tac.xcf &
 sleep 5
-pd -rt -jack -channels 2 -open /home/psc/11h11/thereminacrayon/puredata/main.pd &
+pd -rt -jack -channels 2 -alsamidi -mididev 1 -open /home/psc/11h11/thereminacrayon/puredata/main.pd &
 sleep 5
 wmctrl -r main.pd -t 2 &
 wmctrl -r main.pd -b toggle,fullscreen &
