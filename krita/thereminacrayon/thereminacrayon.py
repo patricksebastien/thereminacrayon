351ecd6a6dce83ac0388f8983633654c0eea1609
Patrick Coulombe <patrick@agora-vfx.com>
2018-12-19 22:57:27 -0500

    osc threading

--- krita/thereminacrayon/thereminacrayon.py
+++ krita/thereminacrayon/thereminacrayon.py
@@ -1,40 +1,33 @@
 #BBD's Krita Script Starter Feb 2018
 import sys
-import liblo
+import threading
+from pythonosc import dispatcher
+from pythonosc import osc_server
 from krita import Extension
 
 EXTENSION_ID = 'pykrita_thereminacrayon'
 MENU_ENTRY = 'thereminacrayon'
-
-global server
 
 class Thereminacrayon(Extension):
 
     def __init__(self, parent):
         #Always initialise the superclass, This is necessary to create the underlying C++ object 
         super().__init__(parent)
-        print("----------------------------------------")
-        print("----------------------------------------")
-        print(sys.version)
-        print("---------------INIT OSC-----------------")
-        try:
-            server = liblo.Server(1239)
-        except liblo.ServerError as err:
-            print(err)
-            sys.exit()
-        print("----------------------------------------")
-        print("----------------------------------------")
-        print("----------------------------------------")
-        print("----------------------------------------")
-
-
-        # OF COURSE PUTTING THIS HERE DOESNT WORK...
-        while True:
-            server.recv(100)
-
+        
 
     def setup(self):
-        pass
+        print("---------------PYTHON VERSION-----------------")
+        print(sys.version)
+        print("---------------THEREMINACRAYON-----------------")
+        self.dispatcher = dispatcher.Dispatcher()
+        self.dispatcher.map("/filter", print)
+        #notworking /volume...
+        self.dispatcher.map("/volume", self.print_compute_handler, "Volume", 2)
+        self.server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 1239), self.dispatcher)
+        print("Serving on {}".format(self.server.server_address))
+        server_thread = threading.Thread(target=self.server.serve_forever)
+        server_thread.start()
+        print("----------------------------------------")
         
     def createActions(self, window):
         action = window.createAction(EXTENSION_ID, MENU_ENTRY, "tools/scripts")
@@ -45,6 +38,14 @@ class Thereminacrayon(Extension):
         
     def action_triggered(self):
         pass # your active code goes here. 
+
+    def print_volume_handler(unused_addr, args, volume):
+        print("[{0}] ~ {1}".format(args[0], volume))
+
+    def print_compute_handler(unused_addr, args, volume):
+        try:
+            print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+        except ValueError: pass
 
 
 # And add the extension to Krita's list of extensions:
