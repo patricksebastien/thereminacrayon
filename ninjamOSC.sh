6bd7b8f9f5a4d98fe7ad6881678e8448bc5f927c
eeight <ununhunun@gmail.com>
2020-04-01 09:37:02 -0400

    ninjam

--- ninjamOSC.sh
+++ ninjamOSC.sh
@@ -1,1 +1,1 @@
-xfce4-terminal -e "/home/psc/11h11/thereminacrayon/ninjamOSC/ninjam/cursesclient/cninjam ninjam.baldapproved.com -user administrator -pass Zaq12wsX -jack -nosavesourcefiles"
+xfce4-terminal -e "/home/psc/11h11/thereminacrayon/ninjam/ninjam/cursesclient/cninjam ninjam.baldapproved.com -user administrator -pass coronavirus -jack -nosavesourcefiles"
